082ff9aa59c50d4081f59e5d4c13fe7e7550b977
davidemess <messori.davide@outlook.it>
2025-02-24 19:09:11 +0100

    MainFormComponent implementation

--- src/app/strongly-typed-reactive-form/components/main-form.component.ts
+++ src/app/strongly-typed-reactive-form/components/main-form.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MainFormFactoryService } from '../services/main-form-factory.service';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserFormComponent } from './user-form.component';
+import { MainForm } from '../models/main-form.interface';
+import { AddressFormComponent } from './address-form.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main-form',
   standalone: true,
-  imports: [],
-  template: ` <p>main-form works!</p> `,
+  imports: [
+    ReactiveFormsModule,
+    UserFormComponent,
+    AddressFormComponent,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
+  template: `
+    <form [formGroup]="form" (ngSubmit)="onSubmit()">
+      <app-user-form [form]="form.controls.userForm"></app-user-form>
+      <app-address-form [form]="form.controls.addressForm"></app-address-form>
+      <button
+        mat-raised-button
+        color="primary"
+        type="submit"
+        [disabled]="form.invalid"
+      >
+        Submit
+      </button>
+    </form>
+  `,
   styles: ``,
 })
-export class MainFormComponent {}
+export class MainFormComponent {
+  private readonly mainFormFactoryService = inject(MainFormFactoryService);
+
+  public form: FormGroup<MainForm> = this.mainFormFactoryService.getForm();
+
+  onSubmit() {
+    console.log(this.form.getRawValue());
+    this.form.reset();
+  }
+}
